6d587afcf5d263b4a16a2bbfa1cf53588656807a
David Goulart <davidgoulart.tae@gmail.com>
2023-04-22 22:51:44 -0300

    Página de checkout estilizada

---
+++ petshop/src/components/product/list/index.js
@@ -0,0 +1,31 @@
+import './styles.css';
+const Product = () => {
+    return (
+        <div className="col-12 product-list">
+            <div className="row">
+                <div className="col-3">
+                    <img src="https://m.media-amazon.com/images/I/517DTupFR3L._AC_SX466_.jpg" alt="Product" className="img-fluid" />
+
+                </div>
+                <div className="col-6">
+                    <h6>
+                        <label className="badge badge-primary">
+                            R$ 30,00
+                        </label>
+                    </h6>
+                    <small>
+                        <b>Ração Pedigree Para Cães Adultos Raças Pequenas 1 kg</b>
+                    </small>
+
+                </div>
+                <div className="col-3">
+                    <button className="btn btn-secondary rounded-circle">-</button>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Product
--- petshop/src/index.js
+++ petshop/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Cadastro from './pages/cadastro';
 import './styles/global.css'
+import Checkout from './pages/checkout';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Cadastro />
+    <Checkout />
   </React.StrictMode>
 );
 
