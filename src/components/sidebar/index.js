976e82e182737c9709582678d40897ddb2152027
David Goulart <davidgoulart.tae@gmail.com>
2023-04-25 18:07:43 -0300

    Sidebar Completa
    Sidebar abre mostrando os produtos, com  scroll e contém botão que a fecha

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,20 +1,18 @@
-import { useState, useEffect } from "react"
+import { FaTimes } from 'react-icons/fa';
 import "./styles.css"
 
 import  Product from "../product/list"
-const Sidebar = () => {
-   const [opened, setOpened] = useState(false)
-   useEffect(() => {
-    window.addEventListener('openCart', () => {
-        setOpened(true)
-    })
-   }, [])
+const Sidebar = (  ) => {
+    const closeSideBar = () => {
+        document.querySelector(".sidebar").style.display = "none"
+    }
    return (
         <div
        
         >
             <div className="container-fluid h-100 pt-4 sidebar">
                 <h5>Minha Sacola (5)</h5>
+                <FaTimes onClick={closeSideBar} className="close-icon" />
                 <div className="row products">
                 <div className="row products">
                                {[1,2,3,4,5,6,7,8,9].map((p) => ( 
